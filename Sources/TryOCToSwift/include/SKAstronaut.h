6495b59653c34599930dba523241d796369ce3c8
Tsungyu Yu <ytyubox@gmail.com>
2020-09-12 14:06:23 +0800

    add unavailable

--- Sources/TryOCToSwift/include/SKAstronaut.h
+++ Sources/TryOCToSwift/include/SKAstronaut.h
@@ -8,9 +8,12 @@
 #ifndef SKAstronaut_h
 #define SKAstronaut_h
 #include <Foundation/Foundation.h>
+NS_ASSUME_NONNULL_BEGIN
 @interface SKAstronaut: NSObject
 - (instancetype) initWithNameConponents:(NSPersonNameComponents *)name NS_DESIGNATED_INITIALIZER;
-- (instancetype) initWithName:(NSString *)name;
-@end
 
+- (instancetype) initWithName:(NSString *)name;
+- (instancetype) init NS_UNAVAILABLE;
+@end
+NS_ASSUME_NONNULL_END
 #endif /* Header_h */
